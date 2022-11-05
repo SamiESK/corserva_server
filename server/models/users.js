eb9aaa09ca732f5d852ae1c2d97fedcb9fbd449a
Sami Eskirjeh <shomee@live.com>
2022-11-04 22:57:29 -0400

    Backend services initialization
    Boiler plate implementation of a RestfulAPI with Sequelize and Node.js instantiations.

---
+++ server/models/users.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const db = require('../util/database');
+
+const User = db.define('users',{
+  uuid: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false,
+    primaryKey: true
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = User;
