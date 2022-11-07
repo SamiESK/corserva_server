e105f4fb2cdf63394590d014aeedf1845ffff369
Sami Eskirjeh <shomee@live.com>
2022-11-07 14:00:22 -0500

    Enabled Cross-origin resource sharing (CORS) for domain sharing.

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const sequelize = require('./util/database');
 const Sale = require('./models/sales');
 
 const app = express();
-
+const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-
+app.options('*', cors())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET','POST','PUT','DELETE');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
 app.use('/sales', require('./routes/sales'));
