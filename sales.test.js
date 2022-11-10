873de22ccc02c4f7546d4e804b13335d4d504289
Sami Eskirjeh <shomee@live.com>
2022-11-10 02:36:53 -0500

    Fixed issues with build after ESLint implementation

---
+++ sales.test.js
@@ -0,0 +1,112 @@
+const { expect } = require('chai');
+const Sale = require('./server/models/sales');
+const salesController = require('./server/controllers/sales');
+const jest = require('jest');
+
+// Test get all sales
+const SalesFindAllMock = async () => {
+    try {
+        const mockData = [
+            {
+                uuid: 'aebd31f5-2594-484a-8afb-3d7b3646d620',
+                productName: 'Product Name',
+                description: 'Product Description',
+                price: 100,
+            },
+            {
+                uuid: 'e3300b1b-f3ad-43e8-b8cf-b75dbcbd7a82',
+                productName: 'Second Product Name',
+                description: 'Second Product Description',
+                price: 200,
+            },
+        ];
+        return res.json({ mockData });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+};
+
+describe('Get all sales requests', () => {
+    it('should return all sales', async () => {
+        // const salesFindAllSpy = jest.spyOn(Sale, 'findAll')
+        // .and.returnValue(
+        //     [
+        //         {
+        //             uuid: 'aebd31f5-2594-484a-8afb-3d7b3646d620',
+        //             productName: 'Product Name',
+        //             description: 'Product Description',
+        //             price: 100,
+        //         },
+        //         {
+        //             uuid: 'e3300b1b-f3ad-43e8-b8cf-b75dbcbd7a82',
+        //             productName: 'Second Product Name',
+        //             description: 'Second Product Description',
+        //             price: 200,
+        //         },
+        //     ]
+        // )
+        let resObject = {};
+        const req = {};
+        const res = {
+            json: jest.fn(),
+            // .mockImplementation((result) => {
+                // resObject = result,
+            // }),
+            status: jest.fn(),
+        };
+        const next = jest.fn();
+        const expectedData = [
+            {
+                uuid: 'aebd31f5-2594-484a-8afb-3d7b3646d620',
+                productName: 'Product Name',
+                description: 'Product Description',
+                price: 100,
+            },
+            {
+                uuid: 'e3300b1b-f3ad-43e8-b8cf-b75dbcbd7a82',
+                productName: 'Second Product Name',
+                description: 'Second Product Description',
+                price: 200,
+            },
+        ];
+        await salesController.getAll(req, res, next);
+        // expect(salesFindAllSpy).toHaveBeenCalled();
+        expect(res.json).toBeCalledWith(expectedData);
+    });
+});
+
+// Test get one sale
+const getOnMock = async (req, res) => {
+    try {
+        const mockData = {
+            uuid: 'aebd31f5-2594-484a-8afb-3d7b3646d620',
+            productName: 'Product Name',
+            description: 'Product Description',
+            price: 100,
+        };
+        return res.json({ mockData });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+};
+
+// describe('Get one sale requests by ID', () => {
+//     it('should return one sale', async () => {
+
+//         const req = {};
+//         const res = {
+//             json: jest.fn().mockImplementation((_) => {
+                
+//             }),
+//         };
+//         const uuid = 'aebd31f5-2594-484a-8afb-3d7b3646d620';
+//         const expectedData = {
+//             uuid: 'aebd31f5-2594-484a-8afb-3d7b3646d620',
+//             productName: 'Product Name',
+//             description: 'Product Description',
+//             price: 100,
+//         };
+//         await getOnMock(req, res);
+//         expect(uuid).to.deep.equal(expectedData.uuid);
+//     });
+// });
--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const { sequelize } = require('./util/database');
-
+const sequelize = require('./util/database');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.options('*', cors());
+app.use(express.urlencoded({ extended: true}));
+app.options('*', cors())
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use('/sales', require('./routes/sales'));
 
-(async () => {
-    try {
-        await sequelize.sync({ force: false });
-        app.listen(process.env.EXTERNAL_PORT || 3001);
-        console.log(`App listening to port ${process.env.EXTERNAL_PORT}`);
-    } catch (error) {
-        console.error(error);
-    }
-})();
-
-module.exports = app;
+(async () =>{
+  try {
+    await sequelize.sync(
+      {force: false}
+    );
+    app.listen(process.env.EXTERNAL_PORT || 3001);
+    console.log("App listening to port " + process.env.EXTERNAL_PORT);
+  } catch (error) {
+    console.error(error);
+  }
+})()
